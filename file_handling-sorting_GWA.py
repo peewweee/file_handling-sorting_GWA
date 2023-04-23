7278612cd1252e0a3ede5f90be80035c3a2b0997
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-23 21:10:04 +0800

    pseudocode

---
+++ file_handling-sorting_GWA.py
@@ -0,0 +1,9 @@
+# File handling - Sorting GWA
+# Phoebe Rhone L. Gangoso | BSCPE 1-4
+
+# open students_gwa file
+    # initialize gwa and student name using lowest possible gwa and student name
+    # read students_gwa file per line and split the student's name and gwa
+        # if gwa < lowest possible grade, get highest gwa and name
+
+    # print the name with highest gwa
