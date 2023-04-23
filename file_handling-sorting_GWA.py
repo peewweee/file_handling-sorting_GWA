1b6480e112a9d31b043966b7a5e213279508c00e
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-23 21:13:04 +0800

    printing line by line in student_gwa file

--- file_handling-sorting_GWA.py
+++ file_handling-sorting_GWA.py
@@ -2,6 +2,9 @@
 # Phoebe Rhone L. Gangoso | BSCPE 1-4
 
 # open students_gwa file
+with open("students_gwa.txt") as input_file:
+    for line in input_file:
+        print(line)
     # initialize gwa and student name using lowest possible gwa and student name
     # read students_gwa file per line and split the student's name and gwa
         # if gwa < lowest possible grade, get highest gwa and name
