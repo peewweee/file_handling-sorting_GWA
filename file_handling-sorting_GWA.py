eda9b8c48e96b80a6b4524ac63db27a5cf12d6c2
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-23 21:33:37 +0800

    printing highest gwa and name

--- file_handling-sorting_GWA.py
+++ file_handling-sorting_GWA.py
@@ -4,10 +4,16 @@
 # open students_gwa file
 with open("students_gwa.txt") as input_file:
     # initialize gwa and student name using lowest possible gwa and student name
+    highest_gwa = 5.0
+    highest_gwa_student = ""
     # read students_gwa file per line and split the student's name and gwa
     for line in input_file:
         name, gwa_str = line.strip().split(" - ")
-        print("Name:", name, "has a GWA of:", gwa_str)
         # if gwa < lowest possible grade, get highest gwa and name
-
+        gwa = float(gwa_str)
+        if gwa < highest_gwa:
+            highest_gwa = gwa
+            highest_gwa_student = name
     # print the name with highest gwa
+    print(highest_gwa)
+    print(highest_gwa_student)
